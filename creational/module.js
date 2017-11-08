5ac5412d6766d5e3372bb6a51ee8d4f05b473771
Parth Desai <psdesai.93@gmail.com>
2017-11-08 00:24:03 -0500

    module pattern

---
+++ creational/module.js
@@ -0,0 +1,22 @@
+//a basic module patter
+
+let testModule = ( () => {
+  let counter = 0;
+  let incrementCounter = () => counter++;
+  let decrementCounter = () => counter--;
+  let resetCounter = () => counter = 0;
+  let count = () => counter;
+  return {
+    incrementCounter: incrementCounter,
+    decrementCounter: decrementCounter,
+    resetCounter: resetCounter,
+    count: count
+  }
+})();
+
+
+testModule.incrementCounter();
+testModule.incrementCounter();
+testModule.decrementCounter();
+testModule.resetCounter();
+
